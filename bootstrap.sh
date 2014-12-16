4ac8b5eec5f9aac8f22e0cbb5cb03f420f9fada8
Matthias Müller-Reineke <mueller-reinekematthias@alice-dsl.de>
2014-12-16 09:34:10 +0100

    bootstrap.sh disables iptables.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+# Without disabling iptables avahi doesn't work properly. See
+# https://bugzilla.redhat.com/show_bug.cgi?id=704809#c9
+chkconfig --del iptables
+service iptables stop
+
 yum install -y avahi
 
 HOSTNAME=${1%%.*}.localdomain
